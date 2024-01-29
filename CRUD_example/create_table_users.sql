4a06b9a96050e3ef9fefd6aec5a5be9457d059a4
mrsferret <mochreach@gmail.com>
2024-01-29 23:12:56 +0000

    Remove example code/ test versions from respository

--- CRUD_example/create_table_users.sql
+++
@@ -1,6 +0,0 @@
-create table users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	phone VARCHAR(15) NOT NULL
-);
--- CRUD_example/insert_users.sql
+++
@@ -1,3 +0,0 @@
-insert into users (email, name, phone)
-VALUES('gianny@dog.com', 'Gianny','07932211765',
-      'nilsa@dog.com', 'Nilsa','07932797565');
