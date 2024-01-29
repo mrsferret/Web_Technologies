27db8f340b60fc044cf6cba8df46152861f8160c
mrsferret <mochreach@gmail.com>
2024-01-29 20:47:24 +0000

    commit tidy up

--- Challenge_1/multi_dimensional_arrays copy.php
+++
@@ -1,7 +0,0 @@
-<?php
-$results = getResults();
-echo $results['jamie']['English'] . <br >"; 
-         
-         echo "Results for Maths for Harry : " ;
-         echo $results['harry']['Maths'] . <br>"; 
-?>
--- Challenge_1/multi_dimensional_arrays.php
+++ Challenge_1/multi_dimensional_arrays.php
@@ -1,40 +1,31 @@
-<?php
-/**
- * @return array[]
- */
-function getResults()
-{
-    echo "<h1>Student Results</h1>";
-    $results = array(
-        "aarron" => array(
+<?php 
+ echo "<h1>Student Results</h1>";
+   $results = array( 
+        "aarron" => array (
             "Physics" => '74%',
-            "English" => '79%',
+            "English" => '79%',	
             "Maths" => '70%'
         ),
 
-        "jamie" => array(
-            "Physics" => '64%',
-            "English" => '79%',
-            "Maths" => '69%'
+        "jamie" => array (
+               "Physics" => '64%',
+               "English" => '79%',	
+               "Maths" => '69%'
         ),
 
-        "harry" => array(
-            "Physics" => '55%',
-            "English " => '52%',
-            "Maths" => '57%'
-        )
-    );
+        "harry" => array (
+               "Physics" =>'55%',
+               "English "=> '52%',	
+               "Maths" => '57%'
+            )
+         );
 
-    /* Accessing multi-dimensional array values */
-    echo "Result for Physics for Aarron : ";
-    echo $results['aarron']['Physics'] . "<br>";
+         /* Accessing multi-dimensional array values */
+         echo "Result for Physics for Aarron : " ;
+         echo $results['aarron']['Physics'] . "<br>"; 
 
-    echo "Results for English for Jamie: ";
-    return $results;
-}
+         echo "Results for English for Jamie: ";
+         echo $results['jamie']['English'] . "<br>"; 
 
-$results = getResults();
-echo $results['jamie']['English'] . "<br >";
-         
          echo "Results for Maths for Harry : " ;
          echo $results['harry']['Maths'] . "<br>";
