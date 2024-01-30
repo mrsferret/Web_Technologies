b8dde8d3c5ff44990f1e5b6312f28f9be2e88371
mrsferret <mochreach@gmail.com>
2024-01-30 00:18:48 +0000

    Remove any superfluous code

--- Challenge_3_CRUD/create_table_products.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `products` (
-	`item_id` INT(10) AUTO_INCREMENT NOT NULL,
-	`item_name` VARCHAR(20) NOT NULL, 
-	`item_desc` VARCHAR(200) NOT NULL, 
-	`item_img` VARCHAR(20) NOT NULL, 
-	`item_price` DECIMAL(4,2) NOT NULL, 
-	PRIMARY KEY (`item_id`)
-	);
